cd8c661b1096988b8b60d281ea68ae9250a02637
Anagha <82244272+anaghajoyis@users.noreply.github.com>
2022-05-25 17:14:35 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 function setup(){
-canvas = createCanvas(550,370);
-canvas.position(550,150);
+    camera=createCapture(VIDEO);
+    camera.size(550,370);
+   var canvas = createCanvas(550,370);
+canvas.position(560,150);
+poseNet = ml5.poseNet(camera,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("Your model is loaded :)");
+}
+function gotPoses(result){
+    if(result.length>0){
+        console.log(result);
+    }
+}
+function draw(){
+background('#bbd0ff');
 }
